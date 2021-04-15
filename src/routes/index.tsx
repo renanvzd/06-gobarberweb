94ad4e033d4b70cf06ae5adf9f98a2d29c752e39
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-14 23:01:43 -0300

    Criando pagina de recuperacao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,16 +5,20 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 
-const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" component={SignUp} />
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
 
-    <Route path="/dashboard" exact component={Dashboard} isPrivate />
-  </Switch>
-);
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+    </Switch>
+  );
+};
 
 export default Routes;
