e1e591cb86c889b25130b7eb5ab5f0d9f5c1628f
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-16 21:25:30 -0300

    Teste

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
 
