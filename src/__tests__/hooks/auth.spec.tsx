0bdc65c7e8c6c97f40c8914115a3d8e210e0c4d6
Renan Veronez Drechsler <renanvz@outlook.com>
2021-06-29 21:03:02 -0300

    Test - Hook: Auth

---
+++ src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,32 @@
+import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
+import { AuthProvider, useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+describe('Auth hook', () => {
+  it('should be able to sign in', async () => {
+    apiMock.onPost('sessions').reply(200, {
+      user: {
+        id: 'user123',
+        name: 'John Doe',
+        email: 'johndoe@example.com.br',
+      },
+      token: 'token-123',
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com.br',
+      password: '123456',
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.user.email).toEqual('johndoe@example.com.br');
+  });
+});
